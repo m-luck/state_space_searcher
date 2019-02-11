1b759c795f6c150e8216d242dc74b958e2a40599
Ashkara Vingh II <michael.j.lukiman@gmail.com>
2019-02-11 10:00:34 -0500

    Transfer to PC

--- tree_maker.py
+++ tree_maker.py
@@ -1,10 +1,8 @@
 from anytree import Node, RenderTree
 # Author: Michael Lukiman. After reading Chang et al. (1994)
-includeRedundantPermutations = True # if permutation found, cut path short
+includeRedundantPermutations = False # if permutation found, cut path short
 includeEmptyProcessorBranch = False # not in production
 sortedHeuristic = False # have the best processors tackle the largest tasks first
-D = 33 # Goal to complete tasks by time D
-S = 110 # Goal to get at least this high of a value within time D
 # =========== A. STATE representation
 waitlist = [] # Tasks needed to be run e.g. [{ 'task': 0, 'computeCost': 12}, { 'task': 1, 'computeCost': 42}] OR just [*task compute times] depending on how human friendly we want the output.
 running = [] # Tasks in the process of running e.g. [{'task': 2, 'onProcessor': 0, 'speed': 2,  finishTime':18}, {'task': 3, 'onProcessor': 1, 'speed': 3, 'computeTimeRemaining': 18, 'startTime':0, 'finishTime':18}]. The more info the easier the debugging.
@@ -16,6 +14,9 @@ value = 0
 # =========== B. START state
 processors = [2,3] # speed
 tasks = [12,42,48,54] # compute time required
+max_val = 0
+for task in tasks:
+    max_val += task
 ind = 0
 taskInd = 0 # to enumerate task IDs at init
 seenPermutations = {}
@@ -23,8 +24,8 @@ for task in tasks:
     waitlist.append({'index':taskInd, 'computeTime':task})
     taskInd += 1
 if sortedHeuristic == True:
-    processors = sorted(processors, reverse=True)
-    waitlist = sorted(waitlist, key= lambda x: x['computeTime'], reverse=True)
+    processors = sorted(processors)
+    waitlist = sorted(waitlist, key= lambda x: x['computeTime'])
 state = {'waitlist':waitlist,'runlist':running,'start':startTime,'earliestFinish':earliestFinishTime,'latestFinish':finalFinishTime, 'value':value, 'IfFinal': value, 'procs': [*processors]} # Clear and effective to be nodes in a tree. Uses unpacking to make sure each state does not refer to the same frozen list.
 root = Node(name=0,value=state)
 # That concludes the initialization.
@@ -40,6 +41,7 @@ def generateChildren():
     global tasks
     global generateList
     global value
+    global max_val
     for state in generateList:
         parent = state # will be added as parent to all children nodes
         generateList.remove(state) # will remove the state from the generate queue before we change its binding
@@ -98,7 +100,11 @@ def generateChildren():
                         addFinalValue += item['len']
                 earliestFinishTime = min(save_runlist, key=lambda x: x['finishTime'])['finishTime']
                 finalFinishTime = max(save_runlist, key=lambda x: x['finishTime'])['finishTime']
-                save_state = { 'waitlist':save_waitlist,'runlist':save_runlist,'start':currentTime,'earliestFinish':earliestFinishTime,'latestFinish':finalFinishTime+currentTime, 'value':oldValue+addValue, 'IfFinal':oldValue+addValue+addFinalValue, 'procs': save_procs}
+                maxMinus = 0
+                for item in save_waitlist:
+                    maxMinus += item['computeTime']
+                newValue = max_val - maxMinus
+                save_state = { 'waitlist':save_waitlist,'runlist':save_runlist,'start':currentTime,'earliestFinish':earliestFinishTime,'latestFinish':finalFinishTime, 'value': newValue, 'IfFinal':oldValue+addValue+addFinalValue, 'procs': save_procs}
                 if includeRedundantPermutations == True:
                     permutation_full = sorted(save_runlist, key=lambda x: x['task'])
                     permutation_tasks = []
@@ -126,29 +132,68 @@ generateChildren()
 # =========== D. GOAL
 # State with empty waitlist, empty runlist, and completion time < D
 # Display tree.
+D = 33 # Goal to complete tasks by time D
+Ds = 25
+S = 110 # Goal to get at least this high of a value within time D
 count = 0
 children = {}
 for pre, fill, node in RenderTree(root):
-    print("{0}NODE {1}\n{0}Waitlist: {2}\n{0}Runlist: {3}\n{0}Start: {4}\n{0}Earliest: {5}\n{0}Latest: {6}\n{0}Value: {7}\n{0}ifFinalValue: {8}".format(pre, node.name, node.value['waitlist'], node.value['runlist'], node.value['start'], node.value['earliestFinish'], node.value['latestFinish'], node.value['value'],node.value['IfFinal']))
+    print("{0}NODE {1}\n{0}Waitlist: {2}\n{0}Runlist: {3}\n{0}Start: {4}\n{0}Earliest: {5}\n{0}Latest: {6}\n{0}Value: {7}".format(pre, node.name, node.value['waitlist'], node.value['runlist'], node.value['start'], node.value['earliestFinish'], node.value['latestFinish'], node.value['value'],node.value['IfFinal']))
     count += 1
     if node.parent in children:
-        children[node.parent].append(node.name)
+        children[node.parent].append(node)
     else:
         children[node.parent] = []
-        children[node.parent].append(node.name)
-print(count)
+        children[node.parent].append(node)
+print("Total nodes:",count)
 
-def dfs_time():
+def search(method=0, question=0):
+    dfsOrbfs = method
+    timeOrTarget = question
+    global root
     global children
+    global D
+    global S
     traversed = []
     stack = [root]
-    while stack:
+    found = False
+    answer = None
+    while stack and found == False:
         cur_node = stack[0]
         stack = stack[1:]
         traversed.append(cur_node)
-        for child in children[cur_node]:
-            stack.insert(0, child)
+        # print(cur_node)
+        if cur_node in children:
+            if dfsOrbfs == 0:
+                for child in children[cur_node]:
+                    stack.insert(0, child)
+            else:
+                for child in children[cur_node]:
+                    stack.append(child)
+        done = 'False'
+        if len(cur_node.value['waitlist']) == 0:
+            done = 'True'
+        print("Latest Finish",cur_node.value['latestFinish'],"| Done:", done, "| Final Value:", cur_node.value['value'])
+        if done == 'True' and cur_node.value['latestFinish'] < D and timeOrTarget == 0:
+            found = True
+            answer=cur_node
+        elif cur_node.value['latestFinish'] < Ds and cur_node.value['value'] > S and timeOrTarget == 1:
+            found = True
+            answer=cur_node
+    count = 0
     for node in traversed:
-        print(node)
+        print(node.name)
+        count += 1
+    print("Total traversed:", count)
+    print("Path:")
+    cur_node = answer
+    print(cur_node,"\n")
+    while cur_node.parent:
+        print(cur_node.parent,"\n")
+        cur_node = cur_node.parent
 
-dfs_time()
+
+search(0,0) # dfs time
+# search(1,0) # bfs time
+# search(0,1) # dfs target
+# search(1,1) # bfs target
