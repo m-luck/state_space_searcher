b0073fc4d6062c33af674151b4aee9b32d5c6149
Ashkara Vingh II <michael.j.lukiman@gmail.com>
2019-02-18 15:42:49 -0500

    parsed input

--- search_via_iterative_deepening.py
+++ search_via_iterative_deepening.py
@@ -1,5 +1,29 @@
-import anytree as at, sys
-path = sys.argv[1] # The 1st option (after this containing file) in cmd should be path to plaintext input
-file = open(path, "r") # Open this path as a file, read-only
-for line in file:
-    line = 
+import anytree as at, re, sys
+tasks = []
+processors = []
+def parseInput(input):
+    global tasks
+    global processors
+    file = open(input, "r")
+    i = 0
+    for line in file:
+        separatedValues = line.split(' ') 
+        for value in separatedValues:
+            valid = re.search(r'^[0-9]+(\.[0-9]+)?$', value)
+            if valid != None:
+                switch = {
+                    0: 
+                    1:
+                    2:
+                }
+        i+=1
+            # assert(float(value)>0)
+def checkInput(): 
+    if len(sys.argv) > 1:
+        path = sys.argv[1] # The 1st option (after this containing file) in cmd should be path to plaintext input
+        valid = re.search(r"^(\/?[&\+\"\'\.a-zA-Z0-9_-]+\/?)+$", path)
+        if valid != None:
+            parseInput(path)
+    else:
+        print("Please supply a path to the input file.")
+checkInput()
